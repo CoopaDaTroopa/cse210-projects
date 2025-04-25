7974c637903810005676a0da590b2d5826666179
Cooper Bench <cooperfbench@gmail.com>
2025-04-25 10:23:00 -0600

    Fininshed hw in prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string lGrade = "";
+        Console.Write("What was your grade percentage? ");
+        int gr = int.Parse(Console.ReadLine());
+        if(gr >= 90){
+            lGrade = "A";
+            Console.WriteLine("YOU PASSED!");
+        } else if(gr >= 80){
+            lGrade = "B";
+            Console.WriteLine("YOU PASSED!");
+        } else if(gr >= 70){
+            lGrade = "C";
+            Console.WriteLine("YOU PASSED!");
+        } else if(gr >= 60){
+            lGrade = "D";
+            Console.WriteLine("You failed. Better luck next time!");
+        } else {
+            lGrade = "F";
+            Console.WriteLine("You failed. Better luck next time!");
+        }
+        Console.WriteLine(lGrade);
     }
 }
