ab0e6e8ede884a32604c5db96cb1d9b59e6c85b6
Cooper Bench <cooperfbench@gmail.com>
2025-05-08 22:26:36 -0600

    homework

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Janitor";
+        job1._endYear = 2025;
+        job1._startYear = 2020;
+        Job job2 = new Job();
+        job2._company = "Lockheed";
+        job2._jobTitle = "Plane Designer";
+        job2._endYear = 2030;
+        job2._startYear = 2025;
+        Resume res = new Resume();
+        res._name = "Cooper Bench";
+        res._jobs.Add(job1);
+        res._jobs.Add(job2);
+        res.DisplayResume();
+
+
+        
     }
 }
