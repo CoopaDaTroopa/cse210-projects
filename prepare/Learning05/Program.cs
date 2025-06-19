f5baa7f10d730577b0e5c10cda6b409f902aef2f
Cooper Bench <cooperfbench@gmail.com>
2025-06-19 16:37:17 -0600

    hw

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Rectangle("Orange", 9, 9.6));
+        shapes.Add(new Square("Pink", 4.3));
+        shapes.Add(new Square("Yellow", 12.9));
+        shapes.Add(new Circle("Blue", 3.1));
+        shapes.Add(new Circle("The Blood of my Enemies", 6.9));
+        shapes.Add(new Rectangle("Magenta", 5, 10.1));
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine($"{s.GetArea()}");
+        }
     }
 }
